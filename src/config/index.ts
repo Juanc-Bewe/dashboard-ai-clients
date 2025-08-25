0a7ca56b6059bc93404bfeef94009d6d5260f298
JuanCJC <juan.ibanez@bewe.io>
2025-08-25 11:45:42 -0500

    Update API base URL in configuration to point to the new production endpoint

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 // Project Configuration
 const config = {
   apiBaseUrl: process.env.NODE_ENV === 'production'
-    ? 'https://prelindabackend.bewe.co'
+    ? 'https://lindabackendpre.bewe.co'
     : 'http://localhost:9010',
 };
 
